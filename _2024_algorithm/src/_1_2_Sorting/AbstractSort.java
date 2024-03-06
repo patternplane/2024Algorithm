1eea4a11b8745185993cd939ee9484124af005f3
patternplane <bss011202@naver.com>
2024-03-07 00:47:56 +0900

    [1주2차시] 수정 : 타입의 지정에 대한 문제 수정

--- _2024_algorithm/src/_1_2_Sorting/AbstractSort.java
+++ _2024_algorithm/src/_1_2_Sorting/AbstractSort.java
@@ -1,25 +1,25 @@
 package _1_2_Sorting;
 
 public abstract class AbstractSort {
-	static public <T> void sort(Comparable<T>[] a) {}
+	static public <T extends Comparable<T>> void sort(T[] a) {}
 	
-	static public <T> boolean less(Comparable<T> a, Comparable<T> b) {
-		return a.compareTo((T) b) < 0;
+	static public <T extends Comparable<T>> boolean less(T a, T b) {
+		return a.compareTo(b) < 0;
 	}
 	
-	static public <T> void exch(Comparable<T>[] a, int i, int j) {
-		Comparable<T> t = a[i];
+	static public <T extends Comparable<T>> void exch(T[] a, int i, int j) {
+		T t = a[i];
 		a[i] = a[j];
 		a[j] = t;
 	}
 	
-	static public <T> void show(Comparable<T>[] a) {
-		for(Comparable<T> item : a)
+	static public <T extends Comparable<T>> void show(T[] a) {
+		for(T item : a)
 			System.out.print(item + " ");
 		System.out.println();
 	}
 	
-	static public <T> boolean isSorted(Comparable<T>[] a) {
+	static public <T extends Comparable<T>> boolean isSorted(T[] a) {
 		for (int i = 0; i < a.length - 1; i++)
 			if (!less(a[i], a[i+1]))
 				return false;
--- _2024_algorithm/src/_1_2_Sorting/Insertion.java
+++ _2024_algorithm/src/_1_2_Sorting/Insertion.java
@@ -1,7 +1,7 @@
 package _1_2_Sorting;
 
 public class Insertion extends AbstractSort {
-	public static <T> void sort(Comparable<T>[] a) {
+	public static <T extends Comparable<T>> void sort(T[] a) {
 		for(int i = 1; i < a.length; i++) {
 			for (int j = i; j > 0; j--)
 				if (less(a[j], a[j-1])) // less가 아니면 for문을 탈출하는 것이 낭비를 줄임

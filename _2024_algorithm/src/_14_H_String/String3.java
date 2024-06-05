84a10e637479dc5db040b7c58a702173969429c3
patternplane <bss011202@naver.com>
2024-06-05 22:37:34 +0900

    [14주 과제] 수정 : 정규식을 활용한 문자열 해석

--- _2024_algorithm/src/_14_H_String/String3.java
+++ _2024_algorithm/src/_14_H_String/String3.java
@@ -6,49 +6,58 @@ package _14_H_String;
 //결과 제출시 : 클래스 명은 Solution
 public class String3 {
 	public int solution(String dartResult) {
-		char[] dr = dartResult.toCharArray();
 
-		int score = 0;
-		int lastScore = 0;
-		int totalScore = 0;
-
-		for (int start = 0, end; start < dr.length;) {
-			end = start;
-
-			while (Character.isDigit(dr[end]))
-				end++;
-			score = charAtoInt(dr, start, end);
-
-			if (dr[end] == 'D')
-				score *= score;
-			else if (dr[end] == 'T')
-				score *= score * score;
-			end++;
-
-			if (end < dr.length && !Character.isDigit(dr[end])) {
-				if (dr[end] == '*') {
-					score *= 2;
-					totalScore += lastScore;
-				} 
-				else if (dr[end] == '#')
-					score = -score;
-				end++;
-			}
-
-			start = end;
-			totalScore += score;
-			lastScore = score;
+		ScoreData scoreData = new ScoreData();
+		
+		int newScore;
+		char bonus;
+		char option;
+		
+		java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("([0-9]*)([SDT])([*#])?");
+		java.util.regex.Matcher matcher = pattern.matcher(dartResult);
+		
+		while (matcher.find()) {
+			newScore = Integer.parseInt(matcher.group(1));
+			bonus = matcher.group(2).charAt(0);
+			option = (matcher.group(3) != null ? matcher.group(3).charAt(0) : '\0');
+			
+			scoreData.takeNextScore(newScore);
+			scoreData.takeBonus(bonus);
+			scoreData.takeOption(option);
+			scoreData.applyCurrentScore();
 		}
 
-		return totalScore;
+		return scoreData.totalScore;
 	}
+}
 
-	int charAtoInt(char[] array, int s, int e) {
-		int result = 0;
-		for (; s < e; s++) {
-			result *= 10;
-			result += array[s] - '0';
+class ScoreData {
+	public int score = 0;
+	public int lastScore = 0;
+	public int totalScore = 0;
+	
+	public void takeNextScore(int score) {
+		this.score = score;
+	}
+	
+	public void takeBonus(char bonus) {
+		if (bonus == 'D')
+			this.score *= this.score;
+		else if (bonus == 'T')
+			this.score *= this.score * this.score;
+	}
+	
+	public void takeOption(char option) {
+		if (option == '*') {
+			this.score *= 2;
+			this.totalScore += this.lastScore;
 		}
-		return result;
+		else if (option == '#')
+			this.score = -this.score;
+	}
+	
+	public void applyCurrentScore() {
+		this.totalScore += this.score;
+		this.lastScore = this.score;
 	}
 }
